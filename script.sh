75bf418ac6edbca45d9e81be57ed495d9d7d1611
Dicami <divillagranpa@uide.edu.ec>
2025-12-04 12:56:38 +0000

    Mi primer script en Arq

---
+++ script.sh
@@ -0,0 +1,6 @@
+#Crear 
+echo "Hola Linux" > archivo.txt #El signo mayor que redirige la salida estandar y comando echo es para imrpimir 
+cat archivo.txt  #Muestra el contenido completo del archivo 
+cp archivo.txt copia.txt #Crea un copia del archivo, archivo de origen en la derecha y la copia a la derecha 
+ls -l  #Muestra directorios y archivos en una carpeta, con -l es listar los archivos
+rm archivo.txt #Elimina el archivo
