bff40bf92e2848355b578a7b9f96f44afd4f7bcd
Dicami <divillagranpa@uide.edu.ec>
2025-12-04 13:22:33 +0000

    Mi segundo script

---
+++ script2.sh
@@ -0,0 +1,4 @@
+uname -a #Se utiliza para imprimir informacion sobre el sistema operativo, el "-a" es el que indica toda la informacion
+lscpu  #Se utiliza para mostrar la arquitectura del procesador de tu sistema.
+free -h #Se utiliza para mostrar la cantidad de memoria física y swap
+df -h #Muestra la cantidad de espacio en disco disponible y utilizado en todos los sistemas de archivos montados.
